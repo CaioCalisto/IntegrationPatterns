88d25deea6229addf2af15c73915e8337873dfb3
Caio <caiocesarcalisto@gmail.com>
2019-04-25 22:14:10 +0100

    First commit

---
+++ RabbitMQ/MessageRouting/MessageRouter/Sender/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exchangeName = "ROUTING_EXCHANGE";
+            IConnection connection = CreateConnection();
+
+            bool continueSending = true;
+            while(continueSending == true)
+            { 
+				// Channel is "Model" in .Net
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct);
+                    DateTime now = DateTime.Now;
+                    string infoMsg = $"Info Message {now}";
+                    string errorMsg = $"Erro Message {now}";
+
+                    byte[] infoBody = Encoding.UTF8.GetBytes(infoMsg);
+                    channel.BasicPublish(exchange: exchangeName, routingKey: "info", basicProperties: null, body: infoBody);
+
+                    byte[] erroBody = Encoding.UTF8.GetBytes(errorMsg);
+                    channel.BasicPublish(exchange: exchangeName, routingKey: "error", basicProperties: null, body: erroBody);
+
+                    Console.WriteLine($"Messages SENT: {infoMsg} - {errorMsg}");
+                }
+                Thread.Sleep(10000);
+            }
+        }
+
+        static IConnection CreateConnection()
+        {
+            IConnection connection = null;
+            IConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "rabbitmq",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672
+            };
+            bool tryAgain = true;
+            while (tryAgain == true)
+            {
+                try
+                {
+                    connection = factory.CreateConnection();
+                    tryAgain = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Thread.Sleep(3000);
+                }
+            }
+            return connection;
+        }
+    }
+}
---
+++ RabbitMQ/MessagingChannels/PublishSubscribeChannel/Receiver2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+namespace Subscribe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exchangeName = "PUBLISHSUBSCRIBE_EXCHANGE";
+            IConnection connection = CreateConnection();
+
+            IModel channel = connection.CreateModel();
+            channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Fanout);
+            string queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "");
+            Console.WriteLine("Receiver2 waiting for messages");
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            consumer.Received += ConsumerReceived;
+            channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+            while (true == true)
+            {
+                Console.WriteLine("Receiver2 listening...");
+                Thread.Sleep(1500);
+            }
+        }
+
+        private static IConnection CreateConnection()
+        {
+            IConnection connection = null;
+            IConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "rabbitmq",
+                UserName = "guest",
+                Password = "guest",
+                Port = 5672
+            };
+            bool tryAgain = true;
+            while (tryAgain == true)
+            {
+                try
+                {
+                    connection = factory.CreateConnection();
+                    tryAgain = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Thread.Sleep(3000);
+                }
+            }
+            return connection;
+        }
+
+        private static void ConsumerReceived(object sender, BasicDeliverEventArgs args)
+        {
+            string message = Encoding.UTF8.GetString(args.Body);
+            Console.WriteLine($"Receiver2: {message}");
+        }
+    }
+}
